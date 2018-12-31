41ea05aa7d1d3101576f0e8f8558878bffb8238f
tal8374 <talivanov@gmail.com>
2018-12-31 10:23:43 +0200

    Javascript Cross Domain problem solved.

--- server/backend/app.js
+++ server/backend/app.js
@@ -18,9 +18,15 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -29,16 +35,16 @@ app.use('/api/bpjs', bpjsRouter);
 app.use('/api/dashboard', dashboardRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  res.status(404).send({ error: 'Not found' })
+app.use(function (req, res, next) {
+    res.status(404).send({error: 'Not found'})
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500).send({ error: err })
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500).send({error: err})
 });
 
 module.exports = app;
