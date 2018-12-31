d36aae1377e234041eacb1632d3777bfd2587960
tal8374 <talivanov@gmail.com>
2018-12-31 12:42:59 +0200

    Handling getting from the flow project the flow, and sending BPThread to the bpjs server.

--- server/backend/bpjs/services/bpjs.service.js
+++ server/backend/bpjs/services/bpjs.service.js
@@ -1,3 +1,53 @@
-module.exports = {
+const request = require('request');
 
+const createBSyncsHandlers = {
+    'action': createBSyncRequest
+};
+
+function createBPProgram(payload) {
+    let flowID = payload.flowID;
+    let flowContent = createBSyncs(payload.flowData);
+
+    let template = `bp.registerBThread(${flowID}, function () { ${flowContent} });`;
+
+    let tosend = {
+        "userEmail": "t@gmail.com",
+        "flowID": flowID,
+        flow: template
+    };
+
+    sendBPProgram(tosend);
+}
+
+function createBSyncs(payload) {
+    let BSyncs = [];
+
+    for (let i = 0; i < payload.length; i++) {
+        let BSync = createBSyncsHandlers[payload[i].type](payload[i]);
+        BSyncs.push(BSync);
+    }
+
+    return BSyncs.join('');
+}
+
+function createBSyncRequest(payload) {
+    let bpID = payload.title;
+
+    return `bp.sync({request: bp.Event('${bpID}')});`;
+}
+
+function sendBPProgram(payload) {
+    request.post(
+        'http://localhost:7000/save',
+        {json: payload},
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body)
+            }
+        }
+    );
+}
+
+module.exports = {
+    createBPProgram
 };
