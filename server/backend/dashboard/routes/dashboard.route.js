f364bf84159d8aa04a49de674db5ecfb67fb0c8d
tal8374 <talivanov@gmail.com>
2019-01-03 12:02:03 +0200

    Creating dashboard rest apis.

--- server/backend/dashboard/routes/dashboard.route.js
+++ server/backend/dashboard/routes/dashboard.route.js
@@ -2,7 +2,14 @@ var express = require('express');
 var router = express.Router();
 const dashboardController = require('../controllers/dashboard.controller');
 
-/* GET users listing. */
-router.get('/list', dashboardController.list);
+router
+    .get('/user/:userEmail/user-profile', dashboardController.getUserProfile)
+    .put('/user/:userEmail/user-profile', dashboardController.updateUserProfile);
+
+router
+    .get('/user/:userEmail/user-course-partners/list', dashboardController.getUserCoursePartners)
+    .get('/user/:userEmail/user-course-partners/:userPartnerID', dashboardController.getUserCoursePartner)
+    .put('/user/:userEmail/user-course-partners/:userPartnerID', dashboardController.updateUserCoursePartner)
+    .delete('/user/:userEmail/user-course-partners/:userPartnerID', dashboardController.deleteUserCoursePartner)
 
 module.exports = router;
