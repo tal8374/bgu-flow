3eb113ac77b4e7fe6a6f84d3de46bc56a0704d8e
tal8374 <talivanov@gmail.com>
2019-02-27 17:04:23 +0200

    Remove irrelevant from menu

--- front-end/flow/src/assets/js/Menus.js
+++ front-end/flow/src/assets/js/Menus.js
@@ -457,7 +457,7 @@ Menus.prototype.init = function()
   })));
   this.put('file', new Menu(mxUtils.bind(this, function(menu, parent)
   {
-    this.addMenuItems(menu, ['new', 'open', '-', 'save', 'saveAs', '-', 'import', 'export'], parent);
+    this.addMenuItems(menu, ['new', 'save'], parent);
   })));
   this.put('edit', new Menu(mxUtils.bind(this, function(menu, parent)
   {
