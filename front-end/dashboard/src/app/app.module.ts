4a2162c070d479e1ca90bf73ea8e724697c98410
tal8374 <talivanov@gmail.com>
2019-04-07 16:35:55 +0300

    Sending to the backend login request

--- front-end/dashboard/src/app/app.module.ts
+++ front-end/dashboard/src/app/app.module.ts
@@ -2,6 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import {AppComponent} from './app.component';
 import {AppRoutes} from './app.routing';
@@ -20,6 +21,7 @@ import {IconsComponent} from './icons/icons.component';
 import {LoginComponent} from './login/login.component';
 import {NotificationsComponent} from './notifications/notifications.component';
 import {UpgradeComponent} from './upgrade/upgrade.component';
+import {HttpModule} from '@angular/http';
 
 @NgModule({
     declarations: [
@@ -38,6 +40,7 @@ import {UpgradeComponent} from './upgrade/upgrade.component';
         BrowserModule,
         RouterModule.forRoot(AppRoutes),
         SidebarModule,
+        HttpModule,
         NavbarModule,
         FooterModule,
         FixedPluginModule,
@@ -45,7 +48,9 @@ import {UpgradeComponent} from './upgrade/upgrade.component';
         NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=YOUR_KEY_HERE'})
 
     ],
-    providers: [],
+    providers: [
+
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
--- front-end/dashboard/src/app/user/user.component.ts
+++ front-end/dashboard/src/app/user/user.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import { Http } from '@angular/http';
 
 @Component({
     selector: 'user-cmp',
@@ -6,7 +8,26 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: 'user.component.html'
 })
 
-export class UserComponent implements OnInit{
-    ngOnInit(){
+export class UserComponent implements OnInit {
+
+    profile = {
+        userName: 'michael23',
+        email: 'email@gmail.com',
+        firstName: 'Chet',
+        lastName: 'Faker',
+    };
+
+    constructor(private http: Http) {
+
+    }
+
+    ngOnInit() {
+    }
+
+    updateProfile() {
+        this.http.put('http://localhost:8000/api/dashboard/user/email', this.profile)
+            .subscribe(res => {
+
+            })
     }
 }
