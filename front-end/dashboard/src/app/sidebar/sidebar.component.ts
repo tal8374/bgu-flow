6b5215a0240730a95fbd3e6331d5e0a32a48c10a
tal8374 <talivanov@gmail.com>
2019-04-07 14:29:25 +0300

    Removing irrelevant options

--- front-end/dashboard/src/app/sidebar/sidebar.component.ts
+++ front-end/dashboard/src/app/sidebar/sidebar.component.ts
@@ -13,11 +13,11 @@ export const ROUTES: RouteInfo[] = [
     { path: 'user', title: 'User Profile',  icon:'ti-user', class: '' },
     { path: 'login', title: 'Login',  icon:'ti-user', class: '' },
     { path: 'course', title: 'Course',  icon:'ti-pencil-alt2', class: '' },
-    { path: 'dashboard', title: 'Dashboard',  icon: 'ti-panel', class: '' },
-    { path: 'table', title: 'Table List',  icon:'ti-view-list-alt', class: '' },
+    // { path: 'dashboard', title: 'Dashboard',  icon: 'ti-panel', class: '' },
+    // { path: 'table', title: 'Table List',  icon:'ti-view-list-alt', class: '' },
     { path: 'flow', title: 'Flow',  icon:'ti-text', class: '' },
-    { path: 'icons', title: 'Icons',  icon:'ti-pencil-alt2', class: '' },
-    { path: 'notifications', title: 'Notifications',  icon:'ti-bell', class: '' },
+    // { path: 'icons', title: 'Icons',  icon:'ti-pencil-alt2', class: '' },
+    // { path: 'notifications', title: 'Notifications',  icon:'ti-bell', class: '' },
     // { path: 'upgrade', title: 'Upgrade to PRO',  icon:'ti-export', class: 'active-pro' },
 ];
 
