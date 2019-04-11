13ec8920e69b6d4f0a169f269d893469fa322270
shachrEli <elshac@post.bgu.ac.il>
2019-04-11 16:21:34 +0300

    .

--- server/backend/bpjs/controllers/bpjs.controller.js
+++ server/backend/bpjs/controllers/bpjs.controller.js
@@ -2,6 +2,7 @@ var iftttService = require('../services/ifttt.service');
 
 function eventLaunch(req, res) {
     // TODO: The name of the event, getting the id from the bpjs and then getting the name from the db.
+    console.log("in eventLanch   ", req.params.componentID);
     let componentID = req.params.componentID;
 
     console.log("Received from the bpjs server event id " + componentID + ' and payload ');
